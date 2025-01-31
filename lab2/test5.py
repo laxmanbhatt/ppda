41ca71fb813adcf24aa3037851539d9977b049a7
laxmanbhatt <bhattlaxman15@gmail.com>
2025-01-31 11:04:54 +0530

    Create test5.py

---
+++ lab2/test5.py
@@ -0,0 +1,11 @@
+def factorial(n)
+ if n==0 or n==1:
+    return 1
+ else:
+    result 1
+    for i in range(1,n+1):
+        result *=1
+    return result  
+num=int(input("eneter a number"))
+print(f"factorial of {num} is {factorial(num)}")
+  
